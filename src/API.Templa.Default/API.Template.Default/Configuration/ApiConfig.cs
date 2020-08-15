32ae4692cbedb0a00ee2b0801a1b5856afd9f5f1
NewBearTed <xprogramador.net@gmail.com>
2020-08-15 15:16:07 -0300

    shortening startup project

---
+++ src/API.Templa.Default/API.Template.Default/Configuration/ApiConfig.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Template.Default.Configuration
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection AddApiConfig(this IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+
+            });
+
+            services.AddVersionedApiExplorer(options =>
+            {
+
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseApiConfig(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            return app;
+        }
+    }
+}
--- src/API.Templa.Default/API.Template.Default/Startup.cs
+++ src/API.Templa.Default/API.Template.Default/Startup.cs
@@ -29,20 +29,9 @@ namespace API.Template.Default
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
 
-            services.AddApiVersioning(options =>
-            {
-                options.ReportApiVersions = true;
-                options.AssumeDefaultVersionWhenUnspecified = true;
 
-            });
-
-            services.AddVersionedApiExplorer(options=> {
-
-                options.GroupNameFormat = "'v'VVV";
-                options.SubstituteApiVersionInUrl = true;
-            });
+            services.AddApiConfig();
 
             // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen();
@@ -58,10 +47,7 @@ namespace API.Template.Default
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseApiConfig(env);
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
@@ -73,17 +59,7 @@ namespace API.Template.Default
                 options.SwaggerEndpoint("/swagger/v1/swagger.json", "Default API");
                 options.RoutePrefix = string.Empty;
             });
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            
         }
     }
 }
