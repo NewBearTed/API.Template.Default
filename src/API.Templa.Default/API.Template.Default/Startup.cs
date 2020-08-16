907b755e389a8223a7977194177905170d31b555
NewBearTed <xprogramador.net@gmail.com>
2020-08-15 21:26:10 -0300

    adding custom middleware

--- src/API.Templa.Default/API.Template.Default/Startup.cs
+++ src/API.Templa.Default/API.Template.Default/Startup.cs
@@ -45,8 +45,7 @@ namespace API.Template.Default
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
-            app.UseMiddleware<RequestLoggingMiddleware>();
+            app.AddCustomMiddleware();
 
             app.UseApiConfig(env);
 
